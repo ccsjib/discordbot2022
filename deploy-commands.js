554252a3be1c94c75e207b3ff7fb4e2e3fe054a2
ccsjib <jsyed@cannonschool.org>
2022-09-22 12:57:18 -0400

    attempting command handling - end of day 2!

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,14 +1,25 @@
-const { REST, SlashCommandBuilder, Routes } = require('discord.js');
+const { REST, Routes } = require('discord.js');
 const { clientId, guildId, token } = require('./config.json');
 
-const commands = [
-	new SlashCommandBuilder().setName('hello').setDescription('Replies with hi!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-]
-	.map(command => command.toJSON());
+const commands = [];
 
 const rest = new REST({ version: '10' }).setToken(token);
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
+	.then(data => console.log(`Successfully registered ${data.length} application commands.`))
+	.catch(console.error);
+
+
+	const fs = require('node:fs');
+const path = require('node:path');
+const { REST, Routes } = require('discord.js');
+const { clientId, guildId, token } = require('./config.json');
+
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	commands.push(command.data.toJSON());
