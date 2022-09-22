4df4a75d0545805ad5bff44ecffd5dd6d28505bc
ccsjib <jsyed@cannonschool.org>
2022-09-22 12:11:24 -0400

    fixed

--- deploy-commands.js
+++ deploy-commands.js
@@ -12,4 +12,3 @@ const rest = new REST({ version: '10' }).setToken(token);
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
 	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
-	.catch(console.error);
