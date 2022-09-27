ff33aaf671ae2cee2fe3c38e715358f8ad4ea17c
ccsjib <jsyed@cannonschool.org>
2022-09-27 15:04:58 -0400

    end of day 3

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,23 +1,32 @@
-const fs = require('node:fs');
-const path = require('node:path');
 const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
+const { token } = require('./config.json');
+const fs = require('node:fs');
 
 const commands = [];
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+// Place your client and guild ids here
+const clientId = '1022213838755594331';
+const guildId = '775355712271941644';
 
 for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
+	const command = require(`./commands/${file}`);
 	commands.push(command.data.toJSON());
 }
 
-
-
 const rest = new REST({ version: '10' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(data => console.log(`Successfully registered ${data.length} application commands.`))
-	.catch(console.error);
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
+		const data = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		console.error(error);
+	}
+})();
